5ebff39991bfa4608974a5116574c7fb2c356a9c
Kevin Chen <chen.kcw@gmail.com>
2021-10-25 11:59:23 -0700

    Generate review model

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,6 +1,7 @@
 class Idea < ApplicationRecord
   belongs_to :user
-  
+  has_many :reviews, dependent: :destroy
+
   validates :title, presence: true, uniqueness: true
   validates :description, length: { minimum: 20 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :ideas, dependent: :destroy
+  has_many :reviews, dependent: :destroy
 
   validates :first_name, :last_name, :email, :password, presence: true
   validates :email, uniqueness: true
