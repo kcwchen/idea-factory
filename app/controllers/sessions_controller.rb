8f657c8e95442e224ac1a5b2f6fcef59fa40d344
Kevin Chen <chen.kcw@gmail.com>
2021-10-25 11:09:47 -0700

    Helper methods for sign in/out and fix nav

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,20 @@
 class SessionsController < ApplicationController
+  
+  def new
+
+  end
+
+  def create
+    @user = User.find_by_email params[:email]
+    if @user&.authenticate params[:password]
+      redirect_to root_path, notice: "Signed In"
+    else
+      flash.now[:alert] = "Wrong email or password"
+      render :new
+    end
+  end
+
+  def destroy
+
+  end
 end
