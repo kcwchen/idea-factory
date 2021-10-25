fff38faaaa74203bc8670ecad8496997be4fecec
Kevin Chen <chen.kcw@gmail.com>
2021-10-25 11:18:09 -0700

    Sign in page and add user references to ideas

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by_email params[:email]
     if @user&.authenticate params[:password]
+      session[:user_id] = @user.id
       redirect_to root_path, notice: "Signed In"
     else
       flash.now[:alert] = "Wrong email or password"
