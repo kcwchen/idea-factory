fc1be84e94c484f5756c4e2b87b8bf3c2922fa4e
Kevin Chen <chen.kcw@gmail.com>
2021-10-25 13:24:33 -0700

    Ability to like

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,21 @@
 class LikesController < ApplicationController
+
+  def create
+    idea = Idea.find params[:idea_id]
+    like = Like.new(idea: idea, user: current_user)
+    if can?(:like, idea)
+      if like.save
+        flash[:notice] = "Idea Liked"
+      else
+        flash[:alert] = like.errors.full_messages.join(", ")
+      end
+    else 
+      flash[:alert] = "You can't like your own idea"
+    end
+    redirect_to root_path
+  end
+
+  def destroy
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get('/', {to:"ideas#index", as: :root})
   resources :ideas do
     resources :reviews, only: [:create, :destroy]
+    resources :likes, shallow: true, only: [:create, :destroy]
   end
   resources :users, only: [:new, :create]
   resource :session, only: [:new, :create, :destroy]
